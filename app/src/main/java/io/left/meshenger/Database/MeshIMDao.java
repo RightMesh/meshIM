c398ae023a258d7709cde438392c327530f904cd
Frazer Seymour <frazer@left.io>
2018-01-24 14:18:30 -0800

    Create simple DAO for creating and querying users.

---
+++ app/src/main/java/io/left/meshenger/Database/MeshIMDao.java
@@ -0,0 +1,20 @@
+package io.left.meshenger.Database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import io.left.meshenger.Models.User;
+
+@Dao
+public interface MeshIMDao {
+    @Insert()
+    public void insertUsers(User... users);
+
+    @Update
+    public void updateUsers(User... users);
+
+    @Query("SELECT * FROM Users")
+    public User[] fetchAllUsers();
+}
