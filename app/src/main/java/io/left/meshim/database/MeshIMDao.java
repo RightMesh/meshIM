7efb299b04ad0ecc8e4b7b3bc372a15fad2e2f5d
Frazer Seymour <frazer@left.io>
2018-02-02 16:53:47 -0800

    Added SQL query to populate Conversation list.

--- app/src/main/java/io/left/meshim/database/MeshIMDao.java
+++ app/src/main/java/io/left/meshim/database/MeshIMDao.java
@@ -5,6 +5,7 @@ import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 import android.arch.persistence.room.Update;
 
+import io.left.meshim.models.ConversationSummary;
 import io.left.meshim.models.MeshIDTuple;
 import io.left.meshim.models.Message;
 import io.left.meshim.models.User;
@@ -33,4 +34,44 @@ public interface MeshIMDao {
     @Query("SELECT * FROM Messages WHERE SenderID IN (:userIds) OR RecipientID IN (:userIds)"
             + " ORDER BY Timestamp ASC")
     Message[] getMessagesBetweenUsers(int... userIds);
+
+    /**
+     * This query is used by {@link io.left.meshim.adapters.UserMessageListAdapter}. That adapter
+     * populates a tab in {@link io.left.meshim.activities.MainTabActivity} which shows a list of
+     * conversations that have been had with other meshIM users in the past.
+     *
+     * <p>
+     *     This query returns a list of {@link ConversationSummary}s, which contain the four fields
+     *     needed to populate a list item in that list: the username and avatar for the user the
+     *     conversation is with, and the message and timestamp of the most recent message exchanged
+     *     with that user.
+     * </p>
+     *
+     * <p>
+     *     This first thing this query does is create a subquery, which shrinks the Messages table
+     *     down to just the most recent message exchanged with each person a message has been
+     *     exchanged with. This is achieved by creating  `PeerID`, which maps to a `User.UserID`
+     *     of a user a message has been exchanged with. `PeerID` is created by shrinking each
+     *     `RecipientID`/`SenderID` pairdown to just the UserID that isn't this device's user. Since
+     *     the latter is always 1, the other user's ID will always be greater in value, so we use
+     *     `max(RecipientID, SenderID)` to acquire the foreign party in the pair. Finally, we use
+     *     `MAX(Timestamp) AS Timestamp` and `GROUP BY PeerID` to filter the table down to the most
+     *     recent message for each peer.
+     * </p>
+     *
+     * <p>
+     *     With the messages sorted, we do an inner join on the Users table where PeerID = UserID in
+     *     order to get the information for the user we've had the conversation with. We go in this
+     *     direction because we will not have exchanged a message with every user in the
+     *     Users table.
+     * </p>
+     * @return a summary of every conversation the device's user has started
+     */
+    @Query("SELECT Username, Avatar, Contents, Timestamp "
+            + "FROM ("
+            + "SELECT max(RecipientID, SenderID) AS PeerID, Contents, MAX(Timestamp) AS Timestamp "
+            + "FROM Messages GROUP BY PeerID"
+            + ") INNER JOIN Users ON PeerID = UserID"
+    )
+    ConversationSummary[] getConversationSummaries();
 }
